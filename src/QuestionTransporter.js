52213bf478ac6bf21d6436553e3b3b97445009af
Crepkey <crepkey@gmail.com>
2019-07-25 10:03:24 +0200

    Basic AJAX call from question API

--- src/QuestionTransporter.js
+++ src/QuestionTransporter.js
@@ -2,17 +2,26 @@ import React, {Component} from 'react';
 import './App.css';
 
 export default class Home extends Component{
+
     constructor(){
         super();
         this.state = {
             questionID: null,
+            questionText: null,
+            answer: null,
+            tags: null,
+            room: null,
             error: null
         }
     }
 
     buildList =(data)=>{
         console.log(data);
-        this.setState({questionID: data.questionID})
+        this.setState({questionID: data.questionID});
+        this.setState({questionText: data.questionText});
+        this.setState({answer: data.answer});
+        this.setState({tags: data.tags});
+        this.setState({room: data.room});
     };
 
     componentDidMount(){
@@ -33,12 +42,28 @@ export default class Home extends Component{
                 <h1>This is HOME</h1>
                 <ul>
                     {
-                        this.state.list.length === 0 &&
+                        this.state.error !== null &&
                         <li>Sorry No data available</li>
                     }
-                    { this.state.list.length > 0 &&
-                        <li>{this.state.list}</li>
-
+                    {
+                        this.state.questionID !== null &&
+                        <li>{this.state.questionID}</li>
+                    }
+                    {
+                        this.state.questionText !== null &&
+                        <li>{this.state.questionText}</li>
+                    }
+                    {
+                        this.state.answer !== null &&
+                        <li>{this.state.answer}</li>
+                    }
+                    {
+                        this.state.tags !== null &&
+                        <li>{this.state.tags}</li>
+                    }
+                    {
+                        this.state.room !== null &&
+                        <li>{this.state.room}</li>
                     }
                 </ul>
                 {this.state.error &&
