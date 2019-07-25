7e3b7e310f7f0385a9ae279a70bc652b23a5a34b
Petrav <vagi.p94@gmail.com>
2019-07-25 11:51:23 +0200

    Test data for addign new question

---
+++ src/AddQuestion.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import './App.css';
+
+export default class AddQuestion extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = {
+            questionText: "YYYYYYYYY",
+            answer: "WHAT",
+            tags: [
+                "OOP",
+                "JAVA"
+            ],
+            room: "OOP"
+        };
+
+        fetch('http://localhost:8080/question/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+            .then(response => console.log('Success:', JSON.stringify(response)))
+            .catch(error => console.error('Error:', error));
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                {/*<label htmlFor="questionText">Enter new question</label>*/}
+                {/*<input id="questionText" name="questionText" type="text" /><br/>*/}
+
+                {/*<label htmlFor="answer">Enter the right answer</label>*/}
+                {/*<input id="answer" name="answer" type="text" /><br/>*/}
+
+                {/*<label htmlFor="room">Choose room for the question</label>*/}
+                {/*<input id="room" name="room" type="text" /><br/>*/}
+
+                <button>Send data!</button>
+            </form>
+        );
+    }
+}
--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import QuestionTransporter from "./QuestionTransporter";
+import AddQuestion from './AddQuestion';
 
 function App() {
   return (
    <div>
-     <QuestionTransporter/>
+     <AddQuestion/>
    </div>
+
+
   );
 }
 
