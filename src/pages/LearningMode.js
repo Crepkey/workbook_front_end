e1c5109e6bc6103e1874dc2a2d864ed43bfbb138
Crepkey <crepkey@gmail.com>
2019-08-07 18:12:56 +0200

    LEARNING MODE: Set the right answer for the question

--- src/pages/LearningMode.js
+++ src/pages/LearningMode.js
@@ -2,30 +2,30 @@ import React, {Component} from 'react';
 import '../App.css';
 import QuestionRating from "../components/QuestionRating";
 
-export default class learningMode extends Component{
-
-    constructor(){
-        super();
-        this.state = {
-            questionID: null,
-            questionText: null,
-            answer: null,
-            tags: null,
-            room: null,
-            hint: false,
-            error: null
-        }
-    }
-
-    buildState =(data)=>{
-        this.setState({questionID: data.questionID});
-        this.setState({questionText: data.questionText});
-        this.setState({answer: data.answer});
-        this.setState({tags: data.tags});
-        this.setState({room: data.room});
+export default class learningMode extends Component {
+    state = {
+        questionID: null,
+        questionText: null,
+        answer: null,
+        tags: null,
+        room: null,
+        hint: false,
+        error: null
     };
 
-    componentDidMount(){
+    buildState = (data) => {
+        this.setState({questionID: data.questionID});
+        this.setState({questionText: data.questionText});
+        this.setState({tags: data.tags});
+        this.setState({room: data.room});
+
+        /*Set Answer*/
+
+        data.answers.filter(answer => answer.rightAnswer)
+            .map(rightAnswer =>  this.setState({answer: rightAnswer.answerText}));
+    };
+
+    componentDidMount() {
         let url = 'http://localhost:8080/question/random';
         fetch(url)
             .then(response => response.json())
@@ -35,32 +35,32 @@ export default class learningMode extends Component{
             })
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <h1 className="App-header">LEARNING MODE</h1>
-                    {
-                        this.state.error !== null &&
-                        <div>Sorry No data available</div>
-                    }
-                    {
-                        this.state.questionText !== null &&
-                        <div><h2>{this.state.questionText}</h2></div>
-                    }
-                        <div>
-                                <button id="check-solution"
-                                        className="red-button margin-10px-from-all-direction"
-                                        onClick={ () => this.setState( {hint: true} ) }>
-                                    Click here for HINT
-                                </button>
-                        </div>
-                    {
-                        this.state.hint === true &&
-                        <div className="answer-border">{this.state.answer}</div>
-                    }
-                    {
-                        this.state.hint === true && <QuestionRating/>
-                    }
+                {
+                    this.state.error !== null &&
+                    <div>Sorry No data available</div>
+                }
+                {
+                    this.state.questionText !== null &&
+                    <div><h2>{this.state.questionText}</h2></div>
+                }
+                <div>
+                    <button id="check-solution"
+                            className="red-button margin-10px-from-all-direction"
+                            onClick={() => this.setState({hint: true})}>
+                        Click here for HINT
+                    </button>
+                </div>
+                {
+                    this.state.hint === true &&
+                    <div className="answer-border">{this.state.answer}</div>
+                }
+                {
+                    this.state.hint === true && <QuestionRating/>
+                }
                 {this.state.error &&
                 <h3>{this.state.error}</h3>
                 }
