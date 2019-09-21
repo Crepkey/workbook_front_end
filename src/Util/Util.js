e689fb152f2b2f6b24b74095e52217a95d88a32b
Crepkey <crepkey@gmail.com>
2019-09-21 18:26:53 +0200

    REFACTORING: Replace the "var" keyword to "let" in UTIL.sleep method

--- src/Util/Util.js
+++ src/Util/Util.js
@@ -1,8 +1,8 @@
 export default class Util {
 
     static sleep(milliseconds) {
-        var start = new Date().getTime();
-        for (var i = 0; i < 1e7; i++) {
+        let start = new Date().getTime();
+        for (let i = 0; i < 1e7; i++) {
             if ((new Date().getTime() - start) > milliseconds){
                 break;
             }
